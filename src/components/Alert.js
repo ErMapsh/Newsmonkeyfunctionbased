96cff721b74b3c6d8a05d9c5229ee0c8055dd5e6
Mahesh Mestri <72149385+ErMapsh@users.noreply.github.com>
2021-11-16 11:33:18 +0530

    alert having margin that why alert needs to modify, so do that

--- src/components/Alert.js
+++ src/components/Alert.js
@@ -5,14 +5,11 @@ export default class Alert extends Component {
   render() {
     return (
       <div style={{height:"50px"}}>
-        {this.props.alert && (
-          <div
-            className={`alert alert-${this.props.alert.type} text-center my-3`} 
-            role="alert"
-          >
-            {this.props.UP(this.props.alert.type)} ❤{this.props.alert.msg}❤
-          </div>
-        )}
+      {this.props.alert && (
+        <div class="alert alert-success text-center" role="alert" >
+          {this.props.UP(this.props.alert.type)} ❤{this.props.alert.msg}❤
+        </div>
+      )}
       </div>
     );
   }
